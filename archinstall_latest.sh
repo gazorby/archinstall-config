43758c2919580185ccf1eb0341f78c0214c2d061
gazorby <gazorby@pm.me>
2022-03-24 19:37:21 +0100

    ♻️ refactor: clone archinstall in private folder

--- archinstall_latest.sh
+++ archinstall_latest.sh
@@ -1,5 +1,5 @@
 pacman -Sy --noconfirm; pacman -S --noconfirm git python-pip
 pip uninstall --yes archinstall
-git clone https://github.com/archlinux/archinstall
-cd archinstall
+git clone https://github.com/archlinux/archinstall .archinstall
+cd .archinstall
 python setup.py install
