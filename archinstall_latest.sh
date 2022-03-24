24459a5fcae2eb77991e493888dd53106570e281
gazorby <gazorby@pm.me>
2022-03-24 22:43:01 +0100

    ♻️ refactor: delete scripts

--- archinstall_latest.sh
+++
@@ -1,5 +0,0 @@
-pacman -Sy --noconfirm; pacman -S --noconfirm git python-pip
-pip uninstall --yes archinstall
-git clone https://github.com/archlinux/archinstall .archinstall
-cd .archinstall
-python setup.py install
--- run.sh
+++
@@ -1,1 +0,0 @@
-archinstall --config config.json --creds creds.json --disk_layouts=disk-layouts.json
